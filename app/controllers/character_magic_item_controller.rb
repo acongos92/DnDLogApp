9f53b9d51cf2590236ced6df376fc9f84d9dbb26
acongos92 <acongos92@gmail.com>
2018-07-17 17:36:06 -0400

    fix crash on no magic item input

--- app/controllers/character_magic_item_controller.rb
+++ app/controllers/character_magic_item_controller.rb
@@ -10,15 +10,21 @@ class CharacterMagicItemController < ApplicationController
 
   def buy
     @magic_item = MagicItem.where(name: get_item_name(params[:magic_item]))[0]
-
     @character = Character.find(params[:id])
-    association = CharacterMagicItem.new
-    association.character_id = params[:id]
-    association.magic_item_id = @magic_item.id
-    association.applied_tp = 0
-    association.save
-    flash[:notice] = "Item Added Succesfully!"
-    redirect_to @character
+    puts @character
+    if @magic_item.nil?
+      flash[:error] = "Please use one of the autofill options"
+      @magic_items = MagicItem.all
+      render 'character_magic_item/form'
+    else
+      association = CharacterMagicItem.new
+      association.character_id = params[:id]
+      association.magic_item_id = @magic_item.id
+      association.applied_tp = 0
+      association.save
+      flash[:notice] = "Item Added Succesfully!"
+      redirect_to @character
+    end
   end
 
   def form_params
